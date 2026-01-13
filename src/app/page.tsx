12327f23c63d203e50fb84685ba378db7b2858fd
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2026-01-13 21:57:27 +0600

    update main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const Client = dynamic(() => import("./_client"), { ssr: false });
-
 export default function Page() {
-  return (
-    <main className="mx-auto w-full max-w-7xl py-10">
-      <Client />
-    </main>
-  );
+  return <main className="mx-auto w-full max-w-7xl py-10">Hello, Tests</main>;
 }
